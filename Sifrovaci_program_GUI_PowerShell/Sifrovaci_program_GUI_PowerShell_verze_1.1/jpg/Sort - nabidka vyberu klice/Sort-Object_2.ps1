34525209ad261ea9099f7770541b904da0a0a399
Sifrovaci-Programy <abcdefgh29@protonmail.com>
2025-01-08 09:39:46 +0100

    Add files via upload

---
+++ Sifrovaci_program_GUI_PowerShell/Sifrovaci_program_GUI_PowerShell_verze_1.1/jpg/Sort - nabidka vyberu klice/Sort-Object_2.ps1
@@ -0,0 +1,26 @@
+﻿cls
+
+#$pole = @("09", "10", "01", "02","03" )
+#$pole = @("09", "10", "02", "05", "07", "06", "01", "11", "03", "08", "04", "09")
+$pole = @("09", "10","02", "05","07", "06","01", "11", "03", "08", "04", "09" ,"ac", "ab","aa", "bb", "ba", "bc","cb", "cc", "ca")
+
+$d_pole = $pole.Length -1
+#echo $d_pole
+
+echo "-------- puvodni pole ------------------"
+for ( $aa = 0; $aa -le $d_pole; $aa++ ) {
+echo $pole[$aa]
+}
+
+# muze i sam sebe jako treba a=a+1 apod.
+# nejni potreba davat napr. $pole2 = $pole | Sort-Object
+#$pole =  $pole | Sort-Object -Property Name # neudela nic
+$pole = $pole | Sort-Object # nejlepsi zatim tohle
+
+echo "-------- sedridene pole ------------------"
+for ( $bb = 0; $bb -le $d_pole; $bb++ ) {
+echo $pole[$bb]
+}
+
+sleep 10
+
