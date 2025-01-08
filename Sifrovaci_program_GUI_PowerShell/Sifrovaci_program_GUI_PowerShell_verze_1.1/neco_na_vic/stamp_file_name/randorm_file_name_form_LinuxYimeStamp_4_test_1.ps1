fa0451b8ab310b930a7c32b366a2b21256d788b6
Sifrovaci-Programy <abcdefgh29@protonmail.com>
2025-01-08 09:35:04 +0100

    Add files via upload

---
+++ Sifrovaci_program_GUI_PowerShell/Sifrovaci_program_GUI_PowerShell_verze_1.1/neco_na_vic/stamp_file_name/randorm_file_name_form_LinuxYimeStamp_4_test_1.ps1
@@ -0,0 +1,49 @@
+﻿cls
+
+# podle linux stamp dela nahodnej nazev souboru
+
+for ( $opakuj = 1; $opakuj -le 20; $opakuj++ ) {
+#echo $opakuj
+[string] $stamp = [DateTimeOffset]::Now.ToUnixTimeseconds()
+#echo $stamp
+$d_stamp = $stamp.Length
+$nazev_bat=""
+
+for ($aa = 10; $aa -ge 1; $aa-- ){
+$cast_stamp_int = 97
+$cast_stamp=$stamp.Substring( ($d_stamp -$aa), 1)
+#echo $cast_stamp
+$cast_stamp_int += [int32] $cast_stamp
+$nazev_bat += [char] $cast_stamp_int
+}
+
+#echo $nazev_bat"<"
+$rnd_znak = Get-Random -Minimum 97 -Maximum 122 # a=97, z=122
+#echo $rnd_znak.GetTypeCode()
+# nakonec prida jeste jeden znak, protoze stam se meni po vterine, coz je dlouho
+$nazev_bat += [char] $rnd_znak
+#echo $nazev_bat"<<"
+#echo $nazev_bat.GetTypeCode()
+$nazev_bat += ".bat"
+echo $nazev_bat
+#sleep 1
+#sleep .5
+sleep .6 # 0.6 pozn: vypada to ze 0.5 a mene, zaokrouhlej dolu na 0.0, takze nic vlasne
+}
+
+<#
+# zpusob ktrerej nefunguje, protoze provdepodobnost opakovani dvou stejnejch nazvu za sebou neni nulova
+# kdezto pri pouziti toho u toho stampu nulova je ( ten de jenom "dopredu" )
+$nazev_bat=""
+for ($aa = 1; $aa -le 5; $aa++){
+$rnd = Get-Random -Minimum 97 -Maximum 122 # a=97, z=122
+$nazev_bat += [char] $rnd
+# [char] je charakter
+}
+$nazev_bat += ".bat"
+echo $nazev_bat
+#>
+
+
+
+
---
+++ Sifrovaci_program_GUI_PowerShell/Sifrovaci_program_GUI_PowerShell_verze_1.1/neco_na_vic/unset_all.ps1
@@ -0,0 +1,77 @@
+﻿cls
+# funguje ! vymaze vsechny uzivatelsky dfinovane premenne ve scriptu
+# tzn. ze opakovany spusteni zacina znovu jako od "nuly" !
+
+# start of script ( store list existing variable names )
+$ExistingVariables = Get-Variable | Select-Object -ExpandProperty Name
+
+<#
+you script here
+#>
+
+echo "---------- list nove nadefinovane  promenne --------------"
+$a=10
+echo $a
+
+$b=20
+echo $b
+
+$c=30
+echo $c
+
+$str_1="ahoj"
+echo $str_1
+
+$d_str_1 = $str_1.Length
+echo $d_str_1
+
+$t_str_1 = $str_1.GetTypeCode()
+echo $t_str_1
+
+$pole_1=@("1","2","3","4")
+echo $pole_1
+
+$pole_2=@(10,20,20,40)
+echo $pole_2
+
+for ($aa=0; $aa -le 5;$aa++){}
+
+for ($aa_1=0; $aa_1 -le 5;$aa_1++){}
+
+for ($bb=0; $bb -le 5;$bb++){}
+
+
+
+# End of script ( remove new variables )
+$NewVariables = Get-Variable | Select-Object -ExpandProperty Name | Where-Object {$ExistingVariables -notcontains $_ -and $_ -ne "ExistingVariables"}
+if ( $NewVariables ) {
+#Write-Host "vymazu vsechny tyto preomenne: `n`n$NewVariables" # to "n$" tady dela newline
+Write-Host "vymazu vsechny tyto promenne: `n`n$NewVariables" # 'n'n$ - nalepeny na sobe neda newline | `n` n$ ( nedela newline )
+Remove-Variable $NewVariables
+} else {
+Write-Host "No new variable to remove !"
+}
+
+echo "zaroven to vlastne dela i soupis vseho co bylo pouzity, protoze kolikrat si clovek uz ani nepamatuje"
+
+echo "--------- list po vymazani zacatek ---------------------"
+echo $a
+echo $b
+
+echo $str_1
+echo $d_str_1
+echo $t_str_1
+
+echo $pole_1
+echo $pole_2
+
+echo $aa
+echo $aa_1
+echo $bb
+
+
+echo "--------- list po vymazani konec -----------------------"
+#sleep 20
+
+
+
