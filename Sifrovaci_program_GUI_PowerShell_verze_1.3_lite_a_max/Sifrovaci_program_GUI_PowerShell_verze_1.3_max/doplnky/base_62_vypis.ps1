141fd5acc4e4d3c1b480d5435a036385179dc013
Sifrovaci-Programy <abcdefgh29@protonmail.com>
2025-01-08 10:47:39 +0100

    Add files via upload

---
+++ Sifrovaci_program_GUI_PowerShell_verze_1.3_lite_a_max/Sifrovaci_program_GUI_PowerShell_verze_1.3_max/doplnky/base_62_vypis.ps1
@@ -0,0 +1,57 @@
+﻿cls
+
+# Base 62 vypis znaku
+
+# definuje pole
+# A=65, Z=90, a=97, z=122, 0=48, 1=49, 9=57
+#$pole_ascii=@(48,57,65,90) # 00-ZZ - Base 32
+$pole_ascii=@(48,57,97,122,65,90) # 00-ZZ Base 62
+$d_pole_ascii=$pole_ascii.Length
+
+$pole_output=@()
+
+$pole=@()
+for ( $cc = 0; $cc -le $d_pole_ascii -2; $cc++ ) {
+$od = $pole_ascii[$cc]
+$do = $pole_ascii[$cc+1]
+for ( $dd = $od; $dd -le $do; $dd++ ) {
+$znk = [char] $dd
+$pole+=$znk
+}
+$cc++
+}
+
+$d_pole = $pole.Length
+echo "Base $d_pole"
+sleep 2
+
+$poc_1 = 0
+for ($i = 0; $i -le 3843; $i++ ){ # 00-ZZ - 3843
+$poc_2 = 0
+for ( $aa = 0; $aa -le $d_pole -1; $aa++) {
+for ( $bb = 0; $bb -le $d_pole -1; $bb++) {
+$poc_2++
+if ( $i -eq $poc_2 -1 ) {
+$x1=$aa
+$x2=$bb
+}
+}
+}
+
+$dvojice=$pole[$x1]
+$dvojice+=$pole[$x2]
+
+$out_1 = [string] $poc_1
+$out_1 += " "
+$out_1 += $dvojice
+echo $out_1
+$pole_output += $out_1
+$poc_1++
+}
+
+sleep 1
+Set-Content -Path "R:\Base_62.txt" -Encoding ASCII -Value $pole_output
+
+sleep 5
+
+
