2a04b3c927233b18e6f6ea7fb6ab2db8ae68d022
Sifrovaci-Programy <abcdefgh29@protonmail.com>
2025-01-09 09:21:57 +0100

    Add files via upload

---
+++ Sifrovaci_program_GUI/Sifrovaci_program_GUI_PowerShell_verze_1.2/doplnky/base_16_vypis.ps1
@@ -0,0 +1,24 @@
+﻿cls
+
+# Base 16 vypis znaku
+
+echo "Base 16"
+sleep 3
+
+$pole_output=@()
+
+for ($int = 0; $int -le 255; $int++){
+$hexa = "{0:x}" -f $int
+$hexa = $hexa.ToUpper()
+if ( $hexa.Length -lt 2 ){
+$hexa = "0" + $hexa
+}
+$out = [string] $int + " " + $hexa
+echo $out
+$pole_output+=$out
+}
+
+sleep 1
+Set-Content -Path "R:\Base_16.txt" -Encoding ASCII -Value $pole_output
+sleep 5
+
