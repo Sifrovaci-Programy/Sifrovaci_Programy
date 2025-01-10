0d8ef2ed3ec809f3a6688b3f149e16a5d3d7c1f1
Martin Pávek <abcdefgh29@protonmail.com>
2025-01-10 09:22:44 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
+# sazeni celeho tohoto repozitare v linuxovim terminalu
 wget -O sifrovaci_programy.zip "https://github.com/Sifrovaci-Programy/Sifrovaci_Programy/archive/refs/heads/main.zip"
