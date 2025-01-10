8e7b01b9e0fd85f182acf737a009504a1ccb0b82
Martin Pávek <abcdefgh29@protonmail.com>
2025-01-10 09:32:58 +0100

    Update stazeni_celeho_repozitare.sh

--- stazeni_celeho_repozitare.sh
+++ stazeni_celeho_repozitare.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
-
 # stazeni celeho tohoto repozitare v linuxovim terminalu pres primy odkaz
-wget -O sifrovaci_programy.zip "https://github.com/Sifrovaci-Programy/Sifrovaci_Programy/archive/refs/heads/main.zip"
+wget -O sifrovaci_programy.zip "https://github.com/Sifrovaci-Programy/Sifrovaci_Programy/archive/refs/heads/main.zip" && unzip -t sifrovaci_programy.zip && unzip -x sifrovaci_programy.zip
 
 # a nebo lze take prikazem "git clone https://github.com/Sifrovaci-Programy/Sifrovaci_Programy"
